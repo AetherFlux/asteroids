a8ab144c25b180dcb3e92bb662ca3f26efe3d355
Lars Thomsen <TheThomsen05@gmail.com>
2024-11-26 21:54:16 +0100

    Setup player model

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     timer = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 10)
 
     while True:
         for event in pygame.event.get():
@@ -13,8 +15,9 @@ def main():
                 return
             
         screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
-        
+
         dt = timer.tick(60) / 1000
 
 if __name__ == "__main__":
