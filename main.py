4254b34a20c93bb387e0a0b8914213ab33be808d
Lars Thomsen <TheThomsen05@gmail.com>
2024-11-27 22:39:44 +0100

    Setup asteroids and spawner

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
+
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     timer = pygame.time.Clock()
     dt = 0
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 10)
+    asteroid_field = AsteroidField()
+    
 
     while True:
         for event in pygame.event.get():
@@ -25,7 +34,7 @@ def main():
 
         for object in drawable:
             object.draw(screen)
-            
+
         pygame.display.flip()
 
         dt = timer.tick(60) / 1000
