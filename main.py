ee0f4613b1222205bcc566eb3b4b586117f3d096
Lars Thomsen <TheThomsen05@gmail.com>
2024-12-12 23:22:20 +0100

    Handle splitting asteroids

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ def main():
             for shot in shots:
                 if shot.detect_collision(asteroid):
                     shot.kill()
-                    asteroid.kill()
+                    asteroid.split()
 
         for object in drawable:
             object.draw(screen)
